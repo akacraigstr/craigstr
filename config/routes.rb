b23add64ae98977c75513dfbe5e0438b6d73f12a
Abby BiYing <abbyzhangnyc@gmail.com>
2014-11-10 16:04:42 -0500

    fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
 
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+  
   constraints Monban::Constraints::SignedIn.new do
-    root "users#show"
+    root "users#show", as: :home
   end
 
   constraints Monban::Constraints::SignedOut.new do
