f9e7304848c9721be9cc69f46bcfc24beb81763b
Abby BiYing <abbyzhangnyc@gmail.com>
2014-11-10 15:29:39 -0500

    fixed stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base 
+class ApplicationController < ActionController::Base
   include Monban::ControllerHelpers
   
   protect_from_forgery with: :exception
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
   constraints Monban::Constraints::SignedIn.new do
-    root "users#show", as: :dashboard
+    root "users#show"
   end
 
   constraints Monban::Constraints::SignedOut.new do
