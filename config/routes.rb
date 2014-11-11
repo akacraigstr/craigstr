1ff0d739d4edd0415144145a98ccfaacf72430de
kaffin <katherin.siracusa@gmail.com>
2014-11-11 16:19:13 -0500

    adding category new page and index page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :posts, only: [:create]
-  resources :categories, only: [:new, :create]
+  resources :categories, only: [:new, :create, :index]
   resources :locations, only: [:new, :create, :index]
 
   constraints Monban::Constraints::SignedIn.new do
