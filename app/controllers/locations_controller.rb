d27b5a60c33140b2aee1b3f88a97c1e6623baeba
kaffin <katherin.siracusa@gmail.com>
2014-11-11 13:01:30 -0500

    Created functionality to add and display locations
    *added create and new actions in locations controller
    *added link to make a new location in admin bar (only visible to admins)
    *added list of locations on index page

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,4 +1,28 @@
-class LocationsController < ApplicationController 
-  def index 
+class LocationsController < ApplicationController
+  def index
+    @locations = Location.all
+  end
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    if current_user.is_admin?
+      @location = Location.new(location_params)
+      if @location.save
+        redirect_to locations_path
+      else
+        render :back
+      end
+    else
+      render :back
+    end
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:name)
   end
 end
