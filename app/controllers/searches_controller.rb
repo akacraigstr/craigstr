8c36b9f9134b7f54ead3d0ded2665cd5c9c1c2b1
kaffin <katherin.siracusa@gmail.com>
2014-11-12 16:01:03 -0500

    adding ability to search for categories

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,5 @@
 class SearchesController < ApplicationController
   def show
-    @posts = Post.where("location_id = ?", params[:location_id])
+    @posts = Post.where("location_id = ? and category_id = ?", params[:location_id], params[:category_id])
   end
 end
